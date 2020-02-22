e68d874f54acca6372b913ea6d3b36534725a8d5
Sebashhdez13 <60381851+Sebashhdez13@users.noreply.github.com>
2020-02-21 20:45:47 -0800

    Peliculas1

---
+++ Pelicula/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+namespace Pelicula
+{
+    class Pelicula
+    {
+        //Atributos
+        public string titulo;
+        public Int16 año;
+        public string pais;
+        public string director;
+
+        //Metodos
+        public void Settitulo(string T)
+        {
+            titulo = T;
+        }
+        public string Gettitulo()
+        {
+            return this.titulo;
+        }
+        public void Setaño(Int16 A)
+        {
+            año = A;
+        }
+        public Int16 Getaño()
+        {
+            return this.año;
+        }
+
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+
+        }
+    }
+}
+
